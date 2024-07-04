7d759d45d9d2b77f216ebc36130094703221fa0d
Vedant Patel <vedantpatel1827@gmail.com>
2024-07-04 19:21:41 +0530

    first commit with gunicorn

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 from application import app 
+
+
+# git remote add origin https://github.com/vedantpatel07756/Billapp.git
+# git branch -M main
+# git push -u origin main
+
+
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/vedantpatel07756/Billapp.git
+# git push -u origin main
