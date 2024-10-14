7ffcfd3a010f9911c79a25ec557be94a7b95b4ca
Vedant Patel <vedantpatel1827@gmail.com>
2024-10-14 23:43:59 +0530

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request,jsonify
+from flask import Flask, render_template,request,jsonify,url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
